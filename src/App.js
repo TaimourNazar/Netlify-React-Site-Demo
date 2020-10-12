82807677bb7d5343d9263000e9081d442a1fc9d7
Taimour <taimour.nazar1@gmail.com>
2020-10-12 18:45:00 +0500

    netlify react updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World BootCamp2020 React Deploy Netlify</h1>
+      <h1>Hello World BootCamp2020 Demo React Deployment on Netlify</h1>
+      <br/>
+      <h3>Netlify URL:</h3>
+      <h4>https://wizardly-cori-7caf37.netlify.app/</h4>
     </div>
   );
 }
